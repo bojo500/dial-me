1e49d80c7b920b7b1b8a0309e6b3e3639706da9b
bojo500 <flavasava1212@gmail.com>
2024-07-17 15:11:28 +0200

    add and fix the database connection logic error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 8886,
+    username: 'admin',
+    password: '^1Wr04yB!NF8',
+    database: 'dialDB',
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
