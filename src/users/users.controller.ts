1e08a955c0bfc5bd20c7e54cb2d3ef3ba556be94
bojo500 <flavasava1212@gmail.com>
2024-07-18 17:42:02 +0200

    add the user module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Body, Patch, Param, Delete,Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto, UpdateUserDto } from "./dto";
+
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.usersService.remove(+id);
+  }
+}
+
