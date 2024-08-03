501aa3e22592cfb4f796eff6658feef7859ccb72
bojo500 <flavasava1212@gmail.com>
2024-08-03 14:53:21 +0300

    Removed ConfigService dependency and switched to direct environment variable access

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,29 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { ConfigService } from "@nestjs/config";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 9001;
+  const port = process.env.PORT || 9001;
+
   const config = new DocumentBuilder()
-    .setTitle('Dial Me Open API 🚀')
-    .setDescription('Dial Me API description 📋')
-    .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('swagger', app, document)
+      .setTitle('Dial Me Open API 🚀')
+      .setDescription('Dial Me API description 📋')
+      .setVersion('1.0')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   app.enableCors({
-  //   // origin: 'http://localhost:3001',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });  app.enableVersioning({
-  //   type: VersioningType.URI,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
 
   await app.listen(port);
   console.log(`Application is running 🚀 on: ${await app.getUrl()} Author : Mohamed Khaled`);
 }
+
 bootstrap();
