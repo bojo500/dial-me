7ba28e981afa41728449ce052591f7b9619f3bb6
bojo500 <flavasava1212@gmail.com>
2024-08-06 14:37:43 +0300

    - handel the error of the database - change the initial variable name from env file - handel the bugs in docker-compose.yml file - add some migration commands in package.json file - add the right rootDir value in tsconfig.json file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,23 @@
 import { DataSource } from 'typeorm';
-import { User } from "./users/entities";
-import { Otp } from "./twilio/entities";
+import { User } from './users/entities';
+import { Otp } from './twilio/entities';
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'test',
-  synchronize: true,
-  logging: false,
-  entities: [User, Otp],
-  migrations: ['src/database/migrations/*.ts'],
+    type: 'mysql',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 8886,
+    username: process.env.DATABASE_USER || 'admin',
+    password: process.env.DATABASE_PASSWORD || '^1Wr04yB!NF8',
+    database: process.env.DATABASE_NAME || 'dialDB',
+    entities: [User, Otp],
+    synchronize: true,
+    logging: true,
 });
+
+AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization:', err);
+    });
